8a83826920f29feccfac9917d4493fb0f56e1151
Git Admin <git@km0lab.com>
2025-11-02 10:26:55 +0100

    feat(chatkit): add deps and chat page

--- src/components/ui/AppHeader.tsx
+++ src/components/ui/AppHeader.tsx
@@ -10,7 +10,7 @@ export function AppHeader({
   className = ""
 }: AppHeaderProps) {
   return (
-    <div className={`w-full px-3.5 py-2 sm:py-4 sm:px-6 md-h:py-6 lg-h:py-8 inline-flex flex-col justify-start items-center ${className}`}>
+    <div className={`w-full px-3.5 py-3 sm:py-12 sm:px-12 md-h:py-6 lg-h:py-8 inline-flex flex-col justify-start items-center ${className}`}>
       <div 
         className="logo-1"
         aria-label={logoAlt}
--- src/components/ui/SliderCount/SliderCount.tsx
+++ src/components/ui/SliderCount/SliderCount.tsx
@@ -15,13 +15,13 @@ const SliderCount: React.FC<SliderCountProps> = ({
   onSkip
 }) => {
   return (
-    <div className="w-80 md-h:w-96 lg-h:w-[28rem] xl-h:w-[32rem] max-w-slide md-h:max-w-96 lg-h:max-w-[28rem] xl-h:max-w-[32rem] mx-auto flex justify-between items-center">
-      <div className="font-body text-lg md-h:text-xl lg-h:text-2xl font-normal text-black">
+    <div className="w-80 sm:w-full sm:max-w-lg sm:mx-auto sm:mt-8 md-h:w-96 lg-h:w-[28rem] xl-h:w-[32rem] max-w-slide sm:max-w-lg md-h:max-w-96 lg-h:max-w-[28rem] xl-h:max-w-[32rem] mx-auto flex justify-between items-center mt-2">
+      <div className="font-body text-lg sm:text-xl md-h:text-xl lg-h:text-2xl font-normal text-black">
         {current}/{total}
       </div>
       <button 
         onClick={onSkip}
-        className="font-body text-lg md-h:text-xl lg-h:text-2xl font-normal text-neutral-900 hover:text-neutral-700 transition-colors"
+        className="font-body text-lg sm:text-xl sm:hover:scale-105 sm:transition-transform md-h:text-xl lg-h:text-2xl font-normal text-neutral-900 hover:text-neutral-700 transition-colors"
       >
         {skipText}
       </button>
